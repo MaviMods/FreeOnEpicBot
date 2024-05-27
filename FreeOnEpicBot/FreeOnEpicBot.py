6ece0d5acb8cf260b5ff7c1d40144b96c6a1b471
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-27 17:30:12 +0530

    Update FreeOnEpicBot.py

--- FreeOnEpicBot/FreeOnEpicBot.py
+++ FreeOnEpicBot/FreeOnEpicBot.py
@@ -41,7 +41,6 @@ def send_message(text):
 		The text to send - the game link
 	"""
 bot = telegram.Bot(token=bot_token)
-  for id in chat_db.values():
         try:
             bot.sendMessage(chat_id=id, text=text)
         except Exception as e:
