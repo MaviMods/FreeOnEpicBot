359be7a5aeaede4551a9c75f6d8136bda5af30fe
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-05-27 17:27:17 +0530

    Update FreeOnEpicBot.py

--- FreeOnEpicBot/FreeOnEpicBot.py
+++ FreeOnEpicBot/FreeOnEpicBot.py
@@ -40,7 +40,7 @@ def send_message(text):
 	Args:
 		The text to send - the game link
 	"""
-    bot = telegram.Bot(token=bot_token)
+bot = telegram.Bot(token=bot_token)
     for id in chat_db.values():
         try:
             bot.sendMessage(chat_id=id, text=text)
